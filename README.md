991f9e0e86c7955d1d137f07a649afa93b57386e
Antonel Dobre <adobre@adobe.com>
2020-07-14 22:04:35 +0300

    Adding PRAW and image recognition libraries
    Signed-off-by: Antonel Dobre <adobre@adobe.com>

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-# Skeleton for Python Environments
-A starting environment for Python3 using virtualenv in order to work.
+# Racial bias detection in a sub reddit
+
+This project aims to determine the racial bias in a Reddit subreddit that is filled with selfies and face pics,
+by determining the skin color of people represented in the selfies and creating a data structure that takes into
+account upvotes and number of comments.
+
+At the moment it can only detect white skin tones, and the detected number out of 1000 selfies of white skin toned characters
+is 784.
+
+It uses PRAW in order to connect with the Reddit API, where it gathers the following data from top submissions of the /r/gaybrosgonemild
+subreddit:
+
+```bash
+author # the author of the post
+skinColor # 1 for white and 0 for non-white ( at least in it's current state )
+score # number of current upvotes
+upvote_ratio # the percentage of upvotes from the total upvotes/downvotes
+comments # the number of top level comments
+created # the date the submission was created in "%Y-%m-%d %H:%M:%S" format
+created_utc # the date the submission was created in UTC format
+```
+
+In order to determine if a picture contains a certain skin tone, it will first detect a face through the OpenCV library dataset in [haarcascade_frontalface_default.xml](haarcascade_frontalface_default.xml).
+
+Once a face has been detected, it takes the image of just the face and applies a skin color mask determined by HSV colors in order to detect
+white skin tone.
+
+If any white skin tone has been detected, it will mark it down into a CSV file with the data mentioned above.
 
 ## How to install
 
@@ -22,6 +48,51 @@ When first activating the virtual environment, run:
 
 ```bash
 make init
+```
+
+Make sure to have a praw.ini file that resembles the following structure, in order to connect to REDDIT API.
+
+```ini
+[DEFAULT]
+# A boolean to indicate whether or not to check for package updates.
+check_for_updates=True
+
+# Object to kind mappings
+comment_kind=t1
+message_kind=t4
+redditor_kind=t2
+submission_kind=t3
+subreddit_kind=t5
+trophy_kind=t6
+
+# The URL prefix for OAuth-related requests.
+oauth_url=https://oauth.reddit.com
+
+# The amount of seconds to ratelimit
+ratelimit_seconds=5
+
+# The URL prefix for regular requests.
+reddit_url=https://www.reddit.com
+
+# The URL prefix for short URLs.
+short_url=https://redd.it
+
+# The timeout for requests to Reddit in number of seconds
+timeout=16
+
+[bot1]
+client_id=######################
+client_secret=######################
+bot_name=RacialBiasGayBrosGoneMild
+bot_version=0.9
+bot_author=######
+user_agent=script:%(bot_name)s:v%(bot_version)s (by /u/%(bot_author)s)
+```
+
+How to run:
+
+```bash
+make run
 ```
 
 ## Project structure
--- app/app.py
+++ app/app.py
@@ -1,5 +1,106 @@
+import praw
+import pprint
+from datetime import datetime
+from urllib.request import urlopen
+import imutils
+import numpy as np
+import cv2
+import csv
+import progressbar
 
+SUBREDDIT="gaybrosgonemild"
 
+def determine_white_skin_color(image):
+    # define the upper and lower boundaries of the HSV pixel
+    # intensities to be considered 'skin'
+    lowerHSV = np.array([0, 48, 80], dtype = "uint8")
+    upperHSV = np.array([20, 255, 255], dtype = "uint8")
+
+    # resize the frame, convert it to the HSV color space,
+    # and determine the HSV pixel intensities that fall into
+    # the speicifed upper and lower boundaries
+    image = imutils.resize(image, width=400)
+    converted = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    skinMask = cv2.inRange(converted, lowerHSV, upperHSV)
+
+    # apply a series of erosions and dilations to the mask
+    # using an elliptical kernel
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+    skinMask = cv2.erode(skinMask, kernel, iterations = 2)
+    skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
+
+    # blur the mask to help remove noise, then apply the
+    # mask to the frame
+    skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
+    skin = cv2.bitwise_and(image, image, mask = skinMask)
+
+    return skin
+
+def determine_face(image):
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    image = imutils.resize(image, width=400)
+    # Convert into grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    maxX = 0
+    maxY = 0
+    maxW = 0
+    maxH = 0
+    aria = -1
+    # Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+        temp_aria = (x+w) * (y+h)
+        if (temp_aria >= aria):
+            aria = temp_aria
+            maxX = x
+            maxY = y
+            maxW = w
+            maxH = h
+        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    crop_image = image[maxY:maxY+maxH, maxX:maxX+maxW]
+    # Display the output
+    return crop_image
+
+def determine_skin_color(url):
+    req = urlopen(url)
+    image = np.asarray(bytearray(req.read()), dtype=np.uint8)
+    image = cv2.imdecode(image, -1)
+
+    image = determine_face(image)
+    image = determine_white_skin_color(image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    if cv2.countNonZero(gray) == 0:
+        return 0
+
+    # show the skin in the image along with the mask
+    # cv2.imshow("images", image)
+    return 1
 
 def main(args):
-    print("Hello world")
+    reddit = praw.Reddit("bot1", config_interpolation="basic")
+
+    with open('data.csv', 'w', newline='') as file:
+        writer = csv.writer(file, delimiter = '|', quoting=csv.QUOTE_NONNUMERIC)
+        subreddit = reddit.subreddit(SUBREDDIT)
+        writer.writerow(["ID", "author", "is_white", "score", "upvote_ratio", "comments", "created", "created_utc"])
+        whitePerson = 0;
+        for submission in progressbar.progressbar(subreddit.top("year", limit=1000)):
+            row = []
+            row.append(submission.id)
+            row.append(submission.author)
+            try:
+                skinColor = determine_skin_color(submission.url)
+            except:
+                skinColor = 0
+            row.append(skinColor)
+            row.append(submission.score)
+            row.append(submission.upvote_ratio)
+            row.append(submission.comments.__len__())
+            row.append(datetime.utcfromtimestamp(submission.created_utc).strftime("%Y-%m-%d %H:%M:%S"))
+            row.append(submission.created_utc)
+            writer.writerow(row)
+            whitePerson = whitePerson + skinColor;
+        pprint.pprint("From 1 000 posts there have been " + str(whitePerson) + "white people that I could detect.")
